466b1cd711129be9dead8d00b30a39a03cd0f862
tortuga <daniel2001faria@gmail.com>
2022-01-04 13:18:13 +0000

    project

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Assets: https://techwithtim.net/wp-content/uploads/2020/09/assets.zip
 import pygame
 from damas.constantes import LARGURA, ALTURA, CASA_SIZE
 from damas.jogo import Jogo
@@ -8,11 +7,13 @@ FPS = 60
 WIN = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption('Checkers')
 
+
 def get_lin_col_rato(pos):
     x, y = pos
     lin = y // CASA_SIZE
     col = x // CASA_SIZE
     return lin, col
+
 
 def main():
     run = True
@@ -29,14 +30,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 lin, col = get_lin_col_rato(pos)
                 jogo.seleciona(lin, col)
 
         jogo.atualiza()
-    
+
     pygame.quit()
+
 
 main()
